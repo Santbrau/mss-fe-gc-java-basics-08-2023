a1c8d3c35b65f90a58a8214a605d1ba08b6a19d5
Marco Santoro <135845147+Santbrau@users.noreply.github.com>
2023-08-10 02:59:44 +0200

    Add files via upload

---
+++ Ejercicio1/src/ejercicio/ejercicio_1.java
@@ -0,0 +1,25 @@
+package ejercicio;
+
+public class ejercicio_1 {
+
+	public static void main(String[] args) {
+		int numero1 = 10;
+        int numero2 = 5;
+        
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        double division = (double) numero1 / numero2; 
+        int modulo = numero1 % numero2;
+        
+        System.out.println("Número 1: " + numero1);
+        System.out.println("Número 2: " + numero2);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+
+	}
+
+}
