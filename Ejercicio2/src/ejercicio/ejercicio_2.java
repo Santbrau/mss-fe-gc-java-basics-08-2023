4df124aec7359c51a1917e8682b470007afc7172
Marco Santoro <135845147+Santbrau@users.noreply.github.com>
2023-08-10 03:00:05 +0200

    Add files via upload

---
+++ Ejercicio2/src/ejercicio/ejercicio_2.java
@@ -0,0 +1,22 @@
+package ejercicio;
+
+public class ejercicio_2 {
+	
+	public static void main(String[] args) {
+	
+	int n = 5;
+	double a = 10;
+	char c = 8;
+	
+	double suma = n + a;
+	double resta = a - n;
+
+	 System.out.println("N = " + n);
+	 System.out.println("A = " + a);
+	 System.out.println("C = " + c);
+	 System.out.println(suma);
+	 System.out.println(resta);
+	 System.out.println((int)c);
+	
+	}
+}
---
+++ Ejercicio3/src/ejercicio/ejercicio_3.java
@@ -0,0 +1,59 @@
+package ejercicio;
+
+public class ejercicio_3 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		double n = 3;
+		double m = 4;
+		
+		double sumaxy = x + y;
+		double diferenciaxy = x - y;
+		double productoxy = x * y;
+		double cocientexy = x / y;
+		double restoxy = x % y;
+		double sumanm = n + m;
+		double diferencianm = n - m;
+		double productonm = n * m;
+		double cocientenm = n / m;
+		double restonm = n - m;
+		double sumaxn = x + n;
+		double cocienteym = y + m;
+		double restoym = y % m;
+		double doblex = x * 2;
+		double dobley = y * 2;
+		double doblen = n * 2;
+		double doblem = m * 2;
+		double sumaxynm = x + y + n + m;
+		double productoxynm = x * y * n * m;
+		
+		
+		System.out.println("X = " + x);
+		System.out.println("Y = " + y);
+		System.out.println("N = " + n);
+		System.out.println("M = " + m);
+		System.out.println("diferencia x y = " + diferenciaxy);
+		System.out.println("producto x y = " + productoxy);
+		System.out.println("cociente x y = " + cocientexy);
+		System.out.println("resto x y = " + restoxy);
+		System.out.println("suma n m = " + sumanm);
+		System.out.println("diferencia n m = " + diferencianm);
+		System.out.println("producto n m = " + productonm);
+		System.out.println("cociente n m = " + cocientenm);
+		System.out.println("resto n m = " + restonm);
+		System.out.println("suma x n = " + sumaxn);
+		System.out.println("cociente y m = " + cocienteym);
+		System.out.println("resto y m = " + restoym);
+		System.out.println("doble x = " + doblex);
+		System.out.println("doble y = " + dobley);
+		System.out.println("doble n = " + doblen);
+		System.out.println("doble m = " + doblem);
+		System.out.println("suma x y n m = " + sumaxynm);
+		System.out.println("producto x y n m = " + productoxynm);
+		
+		
+
+	}
+
+}
